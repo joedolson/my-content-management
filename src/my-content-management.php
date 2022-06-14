31ef46a2a200e7ee5af64e296cd82dfda8f0afb8
Joe Dolson <design@joedolson.com>
2022-06-14 10:53:51 -0500

    Update versions, readme

--- src/my-content-management.php
+++ src/my-content-management.php
@@ -17,7 +17,7 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/license/gpl-2.0.txt
  * Domain Path: lang
- * Version:     1.7.3
+ * Version:     1.7.4
  */
 
 /*
@@ -42,7 +42,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$mcm_version = '1.7.3';
+$mcm_version = '1.7.4';
 /**
  * Enable internationalisation
  */
