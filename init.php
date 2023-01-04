f847ee73d4e4a05611d43717446454323251d34a
Joe Dolson <design@joedolson.com>
2023-01-03 19:00:46 -0600

    Update copyright

---
+++ init.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * My Content Management, Custom Post Type manager for WordPress
+ *
+ * @package     MyContentManagement
+ * @author      Joe Dolson
+ * @copyright   2011-2023 Joe Dolson
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: My Content Management
+ * Plugin URI:  http://www.joedolson.com/my-content-management/
+ * Description: Creates a set of common custom post types for extended content management: FAQ, Testimonials, people lists, term lists, etc.
+ * Author:      Joseph C Dolson
+ * Author URI:  http://www.joedolson.com
+ * Text Domain: my-content-management
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/license/gpl-2.0.txt
+ * Domain Path: lang
+ * Version:     1.7.5
+ */
+
+/*
+	Copyright 2011-2023  Joe Dolson (email : joe@joedolson.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+include( dirname( __FILE__ ) . '/src/my-content-management.php' );
